c2b610cc984ab36731d42af336d341ff85c267ee
lidor4x4 <lidor4x4@gmail.com>
2023-06-17 10:27:15 +0300

    Fixed env

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ module.exports = {
   env: {
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    BASE_URL: process.env.BASE_URL,
   },
 };
 
